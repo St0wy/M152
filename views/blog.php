9b948cda5b92eaac3e5456cc3eef5f9de8211828
St0wy <stawer1911@gmail.com>
2019-01-28 10:57:37 +0100

    Using Flex instead of Grid CSS

--- views/blog.php
+++ views/blog.php
@@ -4,9 +4,18 @@ include "header.php";
 ?>
 <body>
 <?php include "navbar.php" ?>
-<div class="grid-container">
-    <div class="left-column"></div>
-    <div class="right-column"></div>
+<div class="flex-container">
+    <div class="left-column">
+        <div class="user-info">
+            <img src="img/31-86.jpg" alt="naota" id="user-pic">
+            <p id="blog-name">NOM DE VOTRE BLOG</p>
+        </div>
+    </div>
+    <div class="right-column">
+        <div class="welcome-message">
+            <h1>WELCOME</h1>
+        </div>
+    </div>
 </div>
 </body>
 </html>
