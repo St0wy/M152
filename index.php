0737cc2bf8fa99656b26490e454ce4c28bcfedaf
St0wy <stawer1911@gmail.com>
2019-01-28 09:50:20 +0100

    navbar

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+$pageName = basename($_SERVER['PHP_SELF']);
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>My Blog</title>
+</head>
+<body>
+<div class="grid-container">
+<?php include "view/navbar.php" ?>
+</div>
+</body>
+</html>
---
+++ view/navbar.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+        <li><a <?php if ($pageName == "index.php") {?> class="active" <?php }?> href="index.php">Home</a></li>
+        <li><a <?php if ($pageName == "post.php") {?> class="active" <?php }?> href="post.php">Posts</a></li>
+    </ul>
+</nav>
